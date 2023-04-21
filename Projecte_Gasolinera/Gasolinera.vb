c3718a30aa85bb9081f20b15e631b32fe076b767
Noel000 <72937454+Noel000@users.noreply.github.com>
2023-04-21 15:51:28 +0200

    end of details

--- Projecte_Gasolinera/Gasolinera.vb
+++ Projecte_Gasolinera/Gasolinera.vb
@@ -1,4 +1,13 @@
-﻿Public Class Gasolinera
+﻿Imports System.Data.SqlClient
+Imports System.Security.Cryptography
+Imports System.Text
+
+Public Class Gasolinera
+
+    Private conexion As String = "Data Source=DESKTOP-TPUG9J9\SQLEXPRESS;Initial Catalog=carburant;Integrated Security=True"
+    Dim varBBDDTypeOilReturn As String
+    Dim emailorpassword As String
+
     Dim adminTrue As String
     Dim buttonClicResult As Integer
     Dim typeOfOil As String
@@ -16,25 +25,42 @@
 
         If Button1.Tag = buttonClicResult Then
             typeOfOil = "95"
-            DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
-            DetailOil.Show()
+            'DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
+            'DetailOil.Show()
         ElseIf Button2.Tag = buttonClicResult
             typeOfOil = "98"
-            DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
-            DetailOil.Show()
+            'DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
+            'DetailOil.Show()
         ElseIf Button3.Tag = buttonClicResult
-            typeOfOil = "Diesel"
-            DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
-            DetailOil.Show()
+            typeOfOil = "diesel"
+            'DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
+            'DetailOil.Show()
         ElseIf Button4.Tag = buttonClicResult
-            typeOfOil = "DieselPlus"
-            DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
-            DetailOil.Show()
+            typeOfOil = "dieselPlus"
+            'DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
+            'DetailOil.Show()
         ElseIf Button5.Tag = buttonClicResult
             typeOfOil = "Electric"
-            DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
-            DetailOil.Show()
+            'DetailOil.TranslateVarFunctionTypeOil(typeOfOil)
+            'DetailOil.Show()
         End If
+
+
+        Dim cn As New SqlConnection
+        cn.ConnectionString = conexion
+
+        Dim ds As New DataSet
+        'Dim adaptador As New SqlDataAdapter("Select actual from diposit where nom_carburant = " + varBBDDTypeOilReturn, cn)
+        Dim adaptador As New SqlDataAdapter("Select actual, nom_carburant from diposit", cn)
+        adaptador.Fill(ds, "dades")
+        For i As Integer = 0 To ds.Tables("dades").Rows.Count - 1
+            If ds.Tables("Dades").Rows(i).Item(1).Equals(typeOfOil) Then
+                DetailOil.Show()
+                DetailOil.MyVerticalProgessBar1.Value = ds.Tables("Dades").Rows(i).Item(0)
+            End If
+        Next
+
+
     End Sub
 
 End Class
